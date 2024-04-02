1a51d2c61ee80861fc727c6359efbba4b07594e3
jshashank00 <117938519+jshashank00@users.noreply.github.com>
2024-04-02 17:43:19 -0400

    Add files via upload

---
+++ AquariumApp.h
@@ -0,0 +1,25 @@
+/**
+ * @file AquariumApp.h
+ * @author Shashank Jayaram
+ *
+ *
+ */
+
+#ifndef AQUARIUM__AQUARIUMAPP_H
+#define AQUARIUM__AQUARIUMAPP_H
+
+
+
+/**
+ * Aquarium App class. Part of wxWidgets
+ */
+class AquariumApp : public wxApp
+{
+private:
+
+public:
+    bool OnInit() override;
+
+};
+
+#endif //AQUARIUM__AQUARIUMAPP_H
---
+++ main.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file main.cpp
+ * @author Shashank Jayaram
+ */
+#include "pch.h"
+#include "AquariumApp.h"
+
+
+/**
+ * Implements Aquarium App using wxWidget library
+ */
+wxIMPLEMENT_APP(AquariumApp);
